ab4f6bb4edc6cf06fe86c5a51f02275d76418105
raushankumarnitdgp <raushankumar.nitdgp@gmail.com>
2017-08-04 09:22:36 +0530

    All function covered in test

--- scripts/game.js
+++ scripts/game.js
@@ -1,310 +1,300 @@
-var on=false;
-var count=0;
+var on = false;
+var count = 0;
 var timeouts = [];
-var store=[];
-var usr_str=[];
-var cpu_turn=true;
+var store = [];
+var usr_str = [];
+var cpu_turn = true;
 var strict_mode = false;
-var wrng=false;
+var wrng = false;
 
 
-var eleId=["red","green","yellow","blue"];
+var eleId = ["red", "green", "yellow", "blue"];
 
-var bGcolor=["#9f0f17","#00a74a","#cca707","#094a8f"];
+var bGcolor = ["#9f0f17", "#00a74a", "#cca707", "#094a8f"];
 
 
-function reset(){
-	
-	for (var i = 0; i < timeouts.length; i++) {
-		clearTimeout(timeouts[i]);
-	}
-	
-	for(var i=0;i<4;i++)
-		{
-		document.getElementById(eleId[i]).removeEventListener("click",userClick);
-	}
-	
-	document.getElementById("strict").removeEventListener("click", fStrict);
-	document.getElementById("led").style.cssText="background-color: rgb(34, 34, 34)";
-	
-	for(var i=0;i<4;i++)
-		{
-		document.getElementById(eleId[i]).style.cssText="background-color:"+bGcolor[i];
-	}
-	
-	strict_mode=false;
-	
+function reset() {
+
+  for (var i = 0; i < timeouts.length; i++) {
+    clearTimeout(timeouts[i]);
+  }
+
+  for (var i = 0; i < 4; i++) {
+    document.getElementById(eleId[i]).removeEventListener("click", userClick);
+  }
+
+  document.getElementById("strict").removeEventListener("click", fStrict);
+  document.getElementById("led").style.cssText = "background-color: rgb(34, 34, 34)";
+
+  for (var i = 0; i < 4; i++) {
+    document.getElementById(eleId[i]).style.cssText = "background-color:" + bGcolor[i];
+  }
+
+  strict_mode = false;
+
 }
 
-function set(){
-	
-	for(var i=0;i<4;i++)
-		document.getElementById(eleId[i]).addEventListener("click",userClick);
-	
-	document.getElementById("strict").addEventListener("click", fStrict);
+function set() {
+
+  for (var i = 0; i < 4; i++)
+    document.getElementById(eleId[i]).addEventListener("click", userClick);
+
+  document.getElementById("strict").addEventListener("click", fStrict);
 }
 
 
-document.getElementById("count").innerHTML="";
+document.getElementById("count").innerHTML = "";
 
-document.getElementById("OnBtn").addEventListener("change", function(){
-	on=!on;
-	if(on===false)
-		{
-		reset();
-		document.getElementById("start").removeEventListener("click", fstart);
-		document.getElementById("count").innerHTML="";
-	}
-	else
-		{
-		set();
-		document.getElementById("start").addEventListener("click", fstart);
-		document.getElementById("count").innerHTML="--";
-	}
-});
+document.getElementById("OnBtn").addEventListener("change", fOn);
+
+function fOn() {
+  on = !on;
+  if (on === false) {
+    reset();
+    document.getElementById("start").removeEventListener("click", fstart);
+    document.getElementById("count").innerHTML = "";
+  } else {
+    set();
+    document.getElementById("start").addEventListener("click", fstart);
+    document.getElementById("count").innerHTML = "--";
+  }
+}
 
 
 //changing the color on click
-function click_simon(eleId){
-	var colorOn="";
-	var colorBef="";
-	switch(eleId)
-	{
-		case "red":
-		colorOn="background-color:red";
-		colorBef="background-color:#9f0f17";
-		document.getElementById("Sred").play();
-		break;
-		case "green":
-		colorOn="background-color:green";
-		colorBef="background-color:#00a74a";
-		document.getElementById("Sgreen").play();
-		break;
-		case "yellow":
-		colorOn="background-color:yellow";
-		colorBef="background-color:#cca707";
-		document.getElementById("Syellow").play();
-		break;
-		case "blue":
-		colorOn="background-color:blue";
-		colorBef="background-color:#094a8f";
-		document.getElementById("Sblue").play();
-		break;
-	}
-	
-	document.getElementById(eleId).style.cssText = colorOn;
-	timeouts.push(setTimeout(function(){
-		document.getElementById(eleId).style.cssText = colorBef;
-	},400));
-	
+function click_simon(eleId) {
+  var colorOn = "";
+  var colorBef = "";
+  switch (eleId) {
+    case "red":
+      colorOn = "background-color:red";
+      colorBef = "background-color:#9f0f17";
+      document.getElementById("Sred").play();
+      break;
+    case "green":
+      colorOn = "background-color:green";
+      colorBef = "background-color:#00a74a";
+      document.getElementById("Sgreen").play();
+      break;
+    case "yellow":
+      colorOn = "background-color:yellow";
+      colorBef = "background-color:#cca707";
+      document.getElementById("Syellow").play();
+      break;
+    case "blue":
+      colorOn = "background-color:blue";
+      colorBef = "background-color:#094a8f";
+      document.getElementById("Sblue").play();
+      break;
+  }
+
+  document.getElementById(eleId).style.cssText = colorOn;
+  timeouts.push(setTimeout(function() {
+    document.getElementById(eleId).style.cssText = colorBef;
+  }, 400));
+
 }
 
 
 
-function fStrict(){
-	strict_mode=!strict_mode;
-	if(strict_mode===true)
-		document.getElementById("led").style.cssText="background-color:red";
-	else
-		document.getElementById("led").style.cssText="background-color: rgb(34, 34, 34)";
-	//change-led 
+function fStrict() {
+  strict_mode = !strict_mode;
+  if (strict_mode === true)
+    document.getElementById("led").style.cssText = "background-color:red";
+  else
+    document.getElementById("led").style.cssText = "background-color: rgb(34, 34, 34)";
+  //change-led 
 }
 
-function fstart(){
-	if(on===true)
-		startGame();
+function fstart() {
+  if (on === true) {
+    startGame();
+    return 1;
+  } else {
+    return 0;
+  }
+
 }
 
 
-function userClick(event){
-	
-	var eleId=event.target.getAttribute("id");
-	var audId="S"+eleId;
-	
-	var btnNo=5;
-	switch (eleId) {
-		case "red":
-		btnNo=1;
-		break;
-		case "green":
-		btnNo=2;
-		break;
-		case "yellow":
-		btnNo=3;
-		break;
-		case "blue":
-		btnNo=4;
-		break;
-	}
-	
-	if(usr_str.length == store.length || cpu_turn === true)
-		return;
-	
-	usr_str.push(btnNo);
-	
-	if(usr_str[usr_str.length-1]!==store[usr_str.length-1])
-		{
-		comp(store,usr_str);
-	}
-	
-	click_simon(eleId);
+function userClick(event) {
+
+  var eleId = event.target.getAttribute("id");
+  var audId = "S" + eleId;
+
+  var btnNo = 5;
+  switch (eleId) {
+    case "red":
+      btnNo = 1;
+      break;
+    case "green":
+      btnNo = 2;
+      break;
+    case "yellow":
+      btnNo = 3;
+      break;
+    case "blue":
+      btnNo = 4;
+      break;
+  }
+
+  if (usr_str.length == store.length || cpu_turn === true)
+    return 0;
+
+  usr_str.push(btnNo);
+
+  if (usr_str[usr_str.length - 1] !== store[usr_str.length - 1]) {
+    comp(store, usr_str);
+  }
+
+  click_simon(eleId);
+  return 1;
 }
 
 function err() {
-	document.getElementById('Swrong').play();
-	timeouts.push(setTimeout(function(){
-		document.getElementById("count").innerHTML="!!";
-	},100));
-	
-	timeouts.push(setTimeout(function(){
-		if(count<10)
-			document.getElementById("count").innerHTML='0'+count;
-		else
-			document.getElementById("count").innerHTML=count;
-	},700));	
+  document.getElementById('Swrong').play();
+  timeouts.push(setTimeout(function() {
+    document.getElementById("count").innerHTML = "!!";
+  }, 100));
+
+  timeouts.push(setTimeout(function() {
+    if (count < 10)
+      document.getElementById("count").innerHTML = '0' + count;
+    else
+      document.getElementById("count").innerHTML = count;
+  }, 700));
 }
 
 //compares the user input with correct storage
-function comp(store,usr_str) {
-	if(on===false)
-		return 0;
-	
-	if(JSON.stringify(store) === JSON.stringify(usr_str)){
-		addMove();
-	}
-	else{
-		wrng=true;
-		if(strict_mode === true){
-			err();
-			emptyArr();
-			timeouts.push(setTimeout(addMove,500));
-		}
-		else{
-			click_sequence(store,wrng);
-		}
-	}
-	return 1;
+function comp(store, usr_str) {
+  if (on === false)
+    return 0;
+
+  if (JSON.stringify(store) === JSON.stringify(usr_str)) {
+    addMove();
+  } else {
+    wrng = true;
+    if (strict_mode === true) {
+      err();
+      emptyArr();
+      timeouts.push(setTimeout(addMove, 500));
+    } else {
+      click_sequence(store, wrng);
+    }
+  }
+  return 1;
 }
 
 //adds random color move in memory and after adding cpu shows the memory in sequence to player
 function addMove() {
-	
-	if(count>20)
-		{
-		alert("WON!! , Restart the Game");
-		return 0;
-	}
-	
-	
-	var selBtn = Math.floor(Math.random()*5);
-	switch (selBtn) {
-		case 1:
-		store.push(1);
-		break;
-		case 2:
-		store.push(2);
-		break;
-		case 3:
-		store.push(3);
-		break;
-		case 4:
-		store.push(4);
-		break;
-		default:
-		store.push(4);
-		break;
-	}
-	
-	count=store.length;
-	if(count<10)
-		document.getElementById("count").innerHTML='0'+count;
-	else
-		document.getElementById("count").innerHTML=count;
-	
-	wrng=false;
-	click_sequence(store,wrng);
-	
-	return 1;
+
+  if (count > 20) {
+    alert("WON!! , Restart the Game");
+    return 0;
+  }
+
+  var selBtn = Math.floor(Math.random() * 4) + 1;
+
+  switch (selBtn) {
+    case 1:
+      store.push(1);
+      break;
+    case 2:
+      store.push(2);
+      break;
+    case 3:
+      store.push(3);
+      break;
+    case 4:
+      store.push(4);
+      break;
+  }
+
+  count = store.length;
+  if (count < 10)
+    document.getElementById("count").innerHTML = '0' + count;
+  else
+    document.getElementById("count").innerHTML = count;
+
+  wrng = false;
+  click_sequence(store, wrng);
+
+  return 1;
 }
 
 //shows the memory in sequence of clicks
-function click_sequence(store,wrng) {
-	
-	cpu_turn=true;
-	
-	
-	for (var i = 0; i < timeouts.length; i++) {
-		clearTimeout(timeouts[i]);
-	}
-	
-	if(wrng===true)
-		{
-		err();
-		wrng=false;
-	}
-	
-	if(on===false)
-		return 0;
-	
-	var time = 600;
-	for(var i=0;i<store.length;i++)
-		{
-		switch(store[i])
-		{
-			case 1:
-			timeouts.push(setTimeout(click_simon,time,"red"));
-			break;
-			case 2:
-			timeouts.push(setTimeout(click_simon,time,"green"));
-			break;
-			case 3:
-			timeouts.push(setTimeout(click_simon,time,"yellow"));
-			break;
-			case 4:
-			timeouts.push(setTimeout(click_simon,time,"blue"));
-			break;
-		}
-		time+=1000;
-	}
-	
-	while(usr_str.length > 0) {
-		usr_str.pop();
-	}
-	
-	
-	timeouts.push(setTimeout(function(){cpu_turn=false;},time-999));
-	time+=1500;
-	timeouts.push(setTimeout(comp,time+1000*store.length,store,usr_str));
-	
-	
-	
-	return 1;
+function click_sequence(store, wrng) {
+
+  cpu_turn = true;
+
+  for (var i = 0; i < timeouts.length; i++) {
+    clearTimeout(timeouts[i]);
+  }
+
+  if (wrng === true) {
+    err();
+    wrng = false;
+  }
+
+  if (on === false)
+    return 0;
+
+  var time = 600;
+  for (var i = 0; i < store.length; i++) {
+    switch (store[i]) {
+      case 1:
+        timeouts.push(setTimeout(click_simon, time, "red"));
+        break;
+      case 2:
+        timeouts.push(setTimeout(click_simon, time, "green"));
+        break;
+      case 3:
+        timeouts.push(setTimeout(click_simon, time, "yellow"));
+        break;
+      case 4:
+        timeouts.push(setTimeout(click_simon, time, "blue"));
+        break;
+    }
+    time += 1000;
+  }
+
+  while (usr_str.length > 0) {
+    usr_str.pop();
+  }
+
+  timeouts.push(setTimeout(function() {
+    cpu_turn = false;
+  }, time - 999));
+  time += 1500;
+  timeouts.push(setTimeout(comp, time + 1000 * store.length, store, usr_str));
+
+  return 1;
 }
 
 
 //empty the memory
 
 function emptyArr() {
-	while(store.length > 0) {
-		store.pop();
-	}
-	
-	while(usr_str.length > 0) {
-		usr_str.pop();
-	}
-	
+  while (store.length > 0) {
+    store.pop();
+  }
+
+  while (usr_str.length > 0) {
+    usr_str.pop();
+  }
+
 }
 
 //game starts from here ,on start add a random move to sequence array store
 
 function startGame() {
-	
-	reset();
-	if(on===false)
-		return 0;
-	
-	emptyArr();
-	set();
-	addMove();
-	return 1;
+
+  reset();
+  if (on === false)
+    return 0;
+
+  emptyArr();
+  set();
+  addMove();
+  return 1;
 }
--- scripts/spec/simon.spec.js
+++ scripts/spec/simon.spec.js
@@ -1,227 +1,281 @@
+describe("Start Game", function() {
+  beforeEach(function() {
+    spyOn(window, "reset");
+    spyOn(window, "emptyArr");
+    spyOn(window, "set");
+    spyOn(window, "addMove");
+  })
+  it("should start game only if on  is true and calls reset", function() {
+    expect(startGame()).toBe(0);
+    on = true; //game started
+    expect(reset).toHaveBeenCalled();
+    expect(startGame()).toBe(1);
+  })
 
-describe("Start Game",function(){
-        beforeEach(function(){
-            spyOn(window,'reset');
-            spyOn(window,'emptyArr');
-            spyOn(window,'set');
-            spyOn(window,'addMove');
-        })
-        it("should start game only if on  is true and calls reset",function(){
-            expect(startGame()).toBe(0);
-            on=true;    //game started
-            expect(reset).toHaveBeenCalled();
-            //expect(emptyArr).toHaveBeenCalled();
-            expect(startGame()).toBe(1);
-        })
-
-        it("should call emptyArr,set,addMove function",function(){
-            startGame();
-            expect(emptyArr).toHaveBeenCalled();
-            expect(set).toHaveBeenCalled();
-            expect(addMove).toHaveBeenCalled();
-        })
+  it("should call emptyArr,set,addMove function", function() {
+    startGame();
+    expect(emptyArr).toHaveBeenCalled();
+    expect(set).toHaveBeenCalled();
+    expect(addMove).toHaveBeenCalled();
+  })
 })
 
 describe("Empty Arr", function() {
 
-        beforeEach(function(){            
-            store.push(1);
-            usr_str.push(1);
-        })
-        it("should pop all elements from store, usr_str",function(){
-            expect(usr_str.length).toBe(1);
-            emptyArr();
-            expect(usr_str.length).toBe(0);
-            expect(store.length).toBe(0);
-        })
-    
-})
-
-
-describe("Reset",function(){
-    beforeEach(function(){
-        strict_mode=true;
-        timeouts=[1,2];
-    })
-    it("should reset the color",function(){
-        expect(strict_mode).toBe(true);
-        reset();
-        expect(document.getElementById("led").style.cssText).toContain("background-color: rgb(34, 34, 34)");
-        //reseting strict mode
-        expect(strict_mode).toBe(false);
-    })
-})
-
-
-
-describe("Addmove",function(){
-    beforeEach(function(){
-        store=[];
-        count=0;
-    })
-    it("push random value in store array",function(){
-        //intially store length 0
-        expect(store.length).toBe(0);
-        expect(addMove()).toBe(1);
-        expect(document.getElementById("count").innerHTML).toContain("0"+count);
-        //a move just added to store
-    })
-        
-    it("checks possible ,winning condition",function(){
-        //winning condition when count>20
-        count=21;
-        // if count is greater than 21  ,and alert WON
-        expect(addMove()).toBe(0);
-        // no move added
-        expect(store.length).toBe(0);
-    })
-        
-
-
-    it("store a random value at each call ",function(){
-        //below will add 15 moves further
-        for(var i=0;i<15;i++)
-            addMove();
-        console.log(store);
-        expect(store.length).toBe(15);
-        expect(document.getElementById("count").innerHTML).toContain(count);
-    })
-        
+  beforeEach(function() {
+    store.push(1);
+    usr_str.push(1);
+  })
+  it("should pop all elements from store, usr_str", function() {
+    expect(usr_str.length).toBe(1);
+    emptyArr();
+    expect(usr_str.length).toBe(0);
+    expect(store.length).toBe(0);
+  })
 
 })
 
-
-describe("ClickSquence",function(){
-    beforeEach(function(){
-        store=[1,2,3,4];
-        usr_str=[1,2];
-        jasmine.clock().install();
-        cpu_turn=false;
-        wrng=false;
-        count=1;
-    })
-    it("should click sequence present in store array in order",function(){
-        click_sequence(store,wrng);
-        for(var i=0;i<store.length;i++)
-        {
-            switch(store[i])
-            {
-                case 1:
-                        jasmine.clock().tick(600);
-                        expect(document.getElementById("red").style.cssText).toContain("background-color: red;");
-                        jasmine.clock().tick(400);
-                        expect(document.getElementById("red").style.cssText).toContain("background-color: rgb(159, 15, 23);");
-                        break;
-                case 2:
-                        jasmine.clock().tick(600);
-                        expect(document.getElementById("green").style.cssText).toContain("background-color: green;");
-                        jasmine.clock().tick(400);
-                        expect(document.getElementById("green").style.cssText).toContain("background-color: rgb(0, 167, 74);");
-                        break;
-                case 3:
-                        jasmine.clock().tick(600);
-                        expect(document.getElementById("yellow").style.cssText).toContain("background-color: yellow;");
-                        jasmine.clock().tick(400);
-                        expect(document.getElementById("yellow").style.cssText).toContain("background-color: rgb(204, 167, 7);");
-                        break;
-                case 4:
-                        jasmine.clock().tick(600);
-                        expect(document.getElementById("blue").style.cssText).toContain("background-color: blue;");
-                        jasmine.clock().tick(400);
-                        expect(document.getElementById("blue").style.cssText).toContain("background-color: rgb(9, 74, 143);");
-                        break;
-            }
-        }
-    })
-
-    it("should turn cpu_turn flag to true while executing ",function(){
-        expect(cpu_turn).toBe(false);
-        click_sequence(store,wrng);
-        expect(cpu_turn).toBe(true);
-    })
-
-    it("should alert on error , and execute the sequence",function(){
-        wrng=true;
-        click_sequence(store,wrng);
-        jasmine.clock().tick(100);
-        expect(document.getElementById("count").innerHTML).toContain("!!");
-        jasmine.clock().tick(700);
-        expect(document.getElementById("count").innerHTML).toContain("0"+count);
-    })
-
-    it("should return 0 if button is not ON",function(){
-        on=false;
-        expect(click_sequence(store,wrng)).toBe(0);
-    })
-
-    afterEach(function(){
-        jasmine.clock().uninstall();
-    })
+describe("Reset", function() {
+  beforeEach(function() {
+    strict_mode = true;
+    timeouts = [1, 2];
+  })
+  it("should reset the color", function() {
+    expect(strict_mode).toBe(true);
+    reset();
+    expect(document.getElementById("led").style.cssText).toContain("background-color: rgb(34, 34, 34)");
+    //reseting strict mode
+    expect(strict_mode).toBe(false);
+  })
 })
 
-describe("Err",function(){
-    beforeEach(function(){
-        jasmine.clock().install();
-    })
-    it("Prints !! on display board on error",function(){
-        count=1;
-        err();
-        jasmine.clock().tick(100);
-        expect(document.getElementById("count").innerHTML).toContain("!!");
-        jasmine.clock().tick(700);
-        expect(document.getElementById("count").innerHTML).toContain("0"+count);
-    })
+describe("Addmove", function() {
+  beforeEach(function() {
+    store = [];
+    count = 0;
+  })
+  it("push random value in store array", function() {
+    //intially store length 0
+    expect(store.length).toBe(0);
+    expect(addMove()).toBe(1);
+    expect(document.getElementById("count").innerHTML).toContain("0" + count);
+    //a move just added to store
+  })
 
-    it("Prints !! on display board on error ,then count after some time",function(){
-        count=10;
-        err();
-        jasmine.clock().tick(100);
-        expect(document.getElementById("count").innerHTML).toContain("!!");
-        jasmine.clock().tick(700);
-        expect(document.getElementById("count").innerHTML).toContain(count);
-    })
+  it("checks possible ,winning condition", function() {
+    //winning condition when count>20
+    count = 21;
+    // if count is greater than 21  ,and alert WON
+    expect(addMove()).toBe(0);
+    // no move added
+    expect(store.length).toBe(0);
+  })
 
-    afterEach(function(){
-        jasmine.clock().uninstall();
-    })
+
+
+  it("store a random value at each call ", function() {
+    //below will add 15 moves further
+    for (var i = 0; i < 15; i++)
+      addMove();
+    console.log(store);
+    expect(store.length).toBe(15);
+    expect(document.getElementById("count").innerHTML).toContain(count);
+  })
+
+
 })
 
-describe("Compare",function(){
-    beforeAll(function(){
-        store=[1,2,3,4];
-        usr_str=[1,2];
-        on=false;
-        strict_mode=false;
-        wrng=false;
-        spyOn(window,'addMove');
-        spyOn(window,'click_sequence');
-        spyOn(window,'err');
-        spyOn(window,'emptyArr');
-        jasmine.clock().install();
-    })
-    it("should not equal",function(){
-        expect(comp(store,usr_str)).toBe(0);
-        on=true;
-        expect(comp(store,usr_str)).toBe(1);
-        wrng=true;
-        expect(click_sequence).toHaveBeenCalledWith(store,wrng);
-        //testing strict mode
-        strict_mode=true;
-        comp(store,usr_str);
-        expect(err).toHaveBeenCalled();
-        expect(emptyArr).toHaveBeenCalled();
-        expect(addMove).not.toHaveBeenCalled();
-        //addMove will be called after 500 ms
-        jasmine.clock().tick(500);
-        expect(addMove).toHaveBeenCalled();
-    })
-    it("should compare two arrays",function(){
-        usr_str=[1,2,3,4];
-        //testing if arrays equal 
-        comp(store,usr_str);
-        if(JSON.stringify(store) === JSON.stringify(usr_str))
-            expect(addMove).toHaveBeenCalled();
-        //changing usr_str to execute the false part
-    })
-    
+describe("ClickSquence", function() {
+  beforeEach(function() {
+    store = [1, 2, 3, 4];
+    usr_str = [1, 2];
+    jasmine.clock().install();
+    cpu_turn = false;
+    wrng = false;
+    count = 1;
+  })
+  it("should click sequence present in store array in order", function() {
+    click_sequence(store, wrng);
+    for (var i = 0; i < store.length; i++) {
+      switch (store[i]) {
+        case 1:
+          jasmine.clock().tick(600);
+          expect(document.getElementById("red").style.cssText).toContain("background-color: red;");
+          jasmine.clock().tick(400);
+          expect(document.getElementById("red").style.cssText).toContain("background-color: rgb(159, 15, 23);");
+          break;
+        case 2:
+          jasmine.clock().tick(600);
+          expect(document.getElementById("green").style.cssText).toContain("background-color: green;");
+          jasmine.clock().tick(400);
+          expect(document.getElementById("green").style.cssText).toContain("background-color: rgb(0, 167, 74);");
+          break;
+        case 3:
+          jasmine.clock().tick(600);
+          expect(document.getElementById("yellow").style.cssText).toContain("background-color: yellow;");
+          jasmine.clock().tick(400);
+          expect(document.getElementById("yellow").style.cssText).toContain("background-color: rgb(204, 167, 7);");
+          break;
+        case 4:
+          jasmine.clock().tick(600);
+          expect(document.getElementById("blue").style.cssText).toContain("background-color: blue;");
+          jasmine.clock().tick(400);
+          expect(document.getElementById("blue").style.cssText).toContain("background-color: rgb(9, 74, 143);");
+          break;
+      }
+    }
+  })
+
+  it("should turn cpu_turn flag to true while executing ", function() {
+    expect(cpu_turn).toBe(false);
+    click_sequence(store, wrng);
+    expect(cpu_turn).toBe(true);
+  })
+
+  it("should alert on error , and execute the sequence", function() {
+    wrng = true;
+    click_sequence(store, wrng);
+    jasmine.clock().tick(100);
+    expect(document.getElementById("count").innerHTML).toContain("!!");
+    jasmine.clock().tick(700);
+    expect(document.getElementById("count").innerHTML).toContain("0" + count);
+  })
+
+  it("should return 0 if button is not ON", function() {
+    on = false;
+    expect(click_sequence(store, wrng)).toBe(0);
+  })
+
+  afterEach(function() {
+    jasmine.clock().uninstall();
+  })
+})
+
+describe("Err", function() {
+  beforeEach(function() {
+    jasmine.clock().install();
+  })
+  it("Prints !! on display board on error", function() {
+    count = 1;
+    err();
+    jasmine.clock().tick(100);
+    expect(document.getElementById("count").innerHTML).toContain("!!");
+    jasmine.clock().tick(700);
+    expect(document.getElementById("count").innerHTML).toContain("0" + count);
+  })
+
+  it("Prints !! on display board on error ,then count after some time", function() {
+    count = 10;
+    err();
+    jasmine.clock().tick(100);
+    expect(document.getElementById("count").innerHTML).toContain("!!");
+    jasmine.clock().tick(700);
+    expect(document.getElementById("count").innerHTML).toContain(count);
+  })
+
+  afterEach(function() {
+    jasmine.clock().uninstall();
+  })
+})
+
+describe("Compare", function() {
+  beforeAll(function() {
+    store = [1, 2, 3, 4];
+    usr_str = [1, 2];
+    on = false;
+    strict_mode = false;
+    wrng = false;
+    spyOn(window, "addMove");
+    spyOn(window, "click_sequence");
+    spyOn(window, "err");
+    spyOn(window, "emptyArr");
+    jasmine.clock().install();
+  })
+  it("should not equal", function() {
+    expect(comp(store, usr_str)).toBe(0);
+    on = true;
+    expect(comp(store, usr_str)).toBe(1);
+    wrng = true;
+    expect(click_sequence).toHaveBeenCalledWith(store, wrng);
+    //testing strict mode
+    strict_mode = true;
+    comp(store, usr_str);
+    expect(err).toHaveBeenCalled();
+    expect(emptyArr).toHaveBeenCalled();
+    expect(addMove).not.toHaveBeenCalled();
+    //addMove will be called after 500 ms
+    jasmine.clock().tick(500);
+    expect(addMove).toHaveBeenCalled();
+  })
+  it("should compare two arrays", function() {
+    usr_str = [1, 2, 3, 4];
+    //testing if arrays equal 
+    comp(store, usr_str);
+    if (JSON.stringify(store) === JSON.stringify(usr_str))
+      expect(addMove).toHaveBeenCalled();
+    //changing usr_str to execute the false part
+  })
+
+})
+
+describe("User Click", function() {
+  beforeEach(function() {
+    set();
+    cpu_turn = false;
+    usr_str = [];
+    store = [1, 2, 3, 4, 5];
+  })
+
+  it("should click the division where user clicks", function() {
+    var event = new Event("click");
+    document.getElementById("red").dispatchEvent(event);
+    expect(userClick(event)).toBe(1);
+    document.getElementById("green").dispatchEvent(event);
+    userClick(event);
+    document.getElementById("yellow").dispatchEvent(event);
+    userClick(event);
+    document.getElementById("blue").dispatchEvent(event);
+    userClick(event);
+  })
+})
+
+describe("On Button", function() {
+  beforeEach(function() {
+    on = false;
+  })
+  it("should switch on if off ...", function() {
+    expect(on).toBe(false);
+    fOn();
+    expect(on).toBe(true);
+    //again switching it off
+    fOn();
+    expect(on).toBe(false);
+  })
+})
+
+describe("Strict Mode Button", function() {
+  beforeEach(function() {
+    strict_mode = false;
+  })
+  it("should turn strict mode on if not ...", function() {
+    expect(strict_mode).toBe(false);
+    fStrict();
+    expect(strict_mode).toBe(true);
+    //again switching it off
+    fStrict();
+    expect(strict_mode).toBe(false);
+  })
+})
+
+describe("Start Button", function() {
+  beforeEach(function() {
+    on = false;
+    spyOn(window, "startGame");
+  })
+  it("should work if switch is on ...", function() {
+    expect(fstart()).toBe(0);
+    on = true;
+    expect(fstart()).toBe(1);
+    expect(startGame).toHaveBeenCalled();
+  })
 })
